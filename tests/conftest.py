2f2b3594e08c281db4b6f0e6f14ada544e95f995
Bryazgin Sergey <sergeybryazgin@gmail.com>
2023-11-09 22:59:10 +0300

    правки для jenkins

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,6 @@ def browser_config():
     browser.config.window_width = '1920'
     browser.config.window_height = '1080'
 
-    yield
-
-    browser.close()
+    # yield
+    #
+    # browser.close()
